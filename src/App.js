77fbebf27b7db66e6476de1a582ab831d41d9f12
Jayna Leitze <jayna.leitze@gmail.com>
2021-03-16 16:00:03 -0500

    login and register tested and working

--- src/App.js
+++ src/App.js
@@ -2,6 +2,12 @@ import logo from "./logo.png";
 import { theme } from "./theme.js";
 import { Grommet, Box } from "grommet";
 import "./App.css";
+import React from "react";
+import { BrowserRouter, Route, Redirect } from "react-router-dom";
+import { ApplicationViews } from "./ApplicationViews";
+import { NavBar } from "./NavBar";
+import { Login } from "./auth/login";
+import { Register } from "./auth/register";
 
 const AppBar = (props) => (
   <Box
@@ -27,6 +33,42 @@ export const ColorStory = () => {
       <AppBar>
         <div height="4rem"></div>
       </AppBar>
+      <Route
+        render={() => {
+          if (localStorage.getItem("cs_user_id")) {
+            return (
+              <>
+                <NavBar />
+                {/* <ApplicationViews /> */}
+              </>
+            );
+          } else {
+            return <Redirect to="/login" />;
+          }
+        }}
+      />
+
+      <Route
+        path="/login"
+        render={() => {
+          if (localStorage.getItem("cs_user_id")) {
+            return <Redirect to="/" />;
+          } else {
+            return <Login />;
+          }
+        }}
+      />
+
+      <Route
+        path="/register"
+        render={() => {
+          if (localStorage.getItem("cs_user_id")) {
+            return <Redirect to="/" />;
+          } else {
+            return <Register />;
+          }
+        }}
+      />
     </Grommet>
   );
 };
