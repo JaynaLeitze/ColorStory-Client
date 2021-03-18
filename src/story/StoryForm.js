5c5ed0c8534ebe3b55ed91293394c0af6da91601
Jayna Leitze <jayna.leitze@gmail.com>
2021-03-18 12:05:14 -0500

    randomColor is implemented and rendering

--- src/story/StoryForm.js
+++ src/story/StoryForm.js
@@ -1,21 +1,44 @@
-import React, {useState, useContext} from 'react'
-import {useForm} from 'react-hook-form'
-import {Route} from 'react-router-dom'
-import {randomColor} from 'randomcolor'
-
+import React, { useState, useContext } from "react";
+import { useForm } from "react-hook-form";
+import { Route } from "react-router-dom";
+import { randomColor } from "randomcolor";
+import { StoryContext } from "./StoryProvider";
 
 export const StoryForm = (props) => {
-    const {register, handleSubmit} = useForm()
+  const { register, handleSubmit } = useForm();
+  const { createStory } = useContext(StoryContext);
+  const color = randomColor();
 
-    const onSubmit = (story) => {
-        story.user = parseInt(localStorage.getItem("cs_user_id"))
-        
-    }
+  const onSubmit = (story) => {
+    story.user = parseInt(localStorage.getItem("cs_user_id"));
+    createStory(story);
+    props.history.push("/mystories");
+  };
 
-    return (
-        <article className="writeStory">
-            <h1>Create your ColorStory</h1>
-            <div style={{width=150, height=auto, backgroundColor=randomColor()}}></div>
-        </article>
-    )
-}
+  return (
+    <article className="writeStory">
+      <h1>Create your ColorStory</h1>
+
+      <form className="storyForm" onSubmit={handleSubmit(onSubmit)}>
+        <div style={{ width: 150, height: 150, backgroundColor: color }}>
+          Color Prompt
+        </div>
+        <label>Write Your Story</label>
+        <textarea
+          name="content"
+          type="text"
+          ref={register}
+          placeholder="Once upon a time"
+        />
+        <label>Title Your Story:</label>
+        <input
+          name="title"
+          type="text"
+          ref={register}
+          placeholder="Burt Macklin, FBI"
+        />
+        <input type="submit" />
+      </form>
+    </article>
+  );
+};
