879e9664636244e523068bee03902c77dcf610d4
Jayna Leitze <jayna.leitze@gmail.com>
2021-03-19 10:25:53 -0500

    color only renders on page load, does not change with all state changes

--- src/story/StoryForm.js
+++ src/story/StoryForm.js
@@ -8,10 +8,17 @@ import { useHover } from "../useHover";
 export const StoryForm = (props) => {
   const { register, handleSubmit } = useForm();
   const { createStory, randomWord, word } = useContext(StoryContext);
-  const color = randomColor();
+  const [toggle, setToggle] = useState(false);
+  const [color, setColor] = useState("");
+
+  console.log(toggle);
 
   useEffect(() => {
     randomWord();
+  }, []);
+  useEffect(() => {
+    const color = randomColor();
+    setColor(color);
   }, []);
 
   console.log(word);
@@ -26,13 +33,12 @@ export const StoryForm = (props) => {
   return (
     <article className="writeStory">
       <h1>Create your ColorStory</h1>
-
+      <div style={{ width: 150, height: 150, backgroundColor: color }}>
+        Color Prompt
+      </div>
       <form className="storyForm" onSubmit={handleSubmit(onSubmit)}>
-        <div style={{ width: 150, height: 150, backgroundColor: color }}>
-          Color Prompt
-        </div>
         <div className="randomWord">
-          <div>Word Prompt:{word.word}</div>;
+          <div>Word Prompt: {word.word}</div>
         </div>
         <label>Write Your Story</label>
         <textarea
@@ -50,11 +56,13 @@ export const StoryForm = (props) => {
         />
         <input
           name="private"
-          type="radio"
+          type="checkbox"
           ref={register}
-          defaultValue="False"
+          value={!toggle}
+          onChange={() => setToggle(!toggle)}
         />
-        Private Story
+        <label>Private</label>
+
         <input type="submit" />
       </form>
     </article>
